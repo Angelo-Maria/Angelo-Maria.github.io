3aa8f127df2762fdb10edc983b1ffd032e3cf622
MariaLazzaroni <maria1.lazzaroni@gmail.com>
2025-01-06 14:40:58 +0100

    added 2 randomly chosen pics for Madagascar

--- js/functionsViaggio.js
+++ js/functionsViaggio.js
@@ -39,18 +39,25 @@ function pickImage(){
     fig_botswana = ["VictoriaFalls3ok.jpg",
         "OkavangoDelta.jpg",
         "ChobeNationalPark.jpg"]
-    // fig_madagascar = ["figures/NosyBe.jpg",
-    //         "figures/NosyBe2.jpg",
-    //         "figures/NosyAntsoha.jpg"]    
+    fig_madagascar = ["NosyBe.jpg",
+        "NosyBe2.jpg",
+        "NosyAntsoha.jpg"]    
     let random_b = Math.floor(Math.random() * fig_botswana.length);
     let random_b2;
         do {
             random_b2 = Math.floor(Math.random() * fig_botswana.length);
-        } while (random_b2 === random_b);        
+        } while (random_b2 === random_b);    
+    let random_m = Math.floor(Math.random() * fig_madagascar.length);
+    let random_m2;
+        do {
+            random_m2 = Math.floor(Math.random() * fig_madagascar.length);
+        } while (random_m2 === random_m);              
     // console.log(random_b);
     // console.log(random_b2);
-    let randomImg_b = "figures/" + fig_botswana[random_b]
+    let randomImg_b  = "figures/" + fig_botswana[random_b]
     let randomImg_b2 = "figures/" + fig_botswana[random_b2]
+    let randomImg_m  = "figures/" + fig_madagascar[random_m]
+    let randomImg_m2 = "figures/" + fig_madagascar[random_m2]
     // randomImg_b = "images/" + options[Math.floor(Math.random() * options.length)];
     // let randomImg_m = fig_madagascar[Math.floor(Math.random() * fig_madagascar.length)];
     // console.log(randomImg_b)
@@ -59,6 +66,13 @@ function pickImage(){
     // console.log(randomImg_b[1]) //vuoto
     img = document.getElementById('Botswana1')
     img.setAttribute("src",randomImg_b)
-    img = document.querySelector('Botswana2')
+    img = document.getElementById('Botswana2')
     img.setAttribute("src",randomImg_b2)
+    img = document.getElementById('Madagascar1')
+    img.setAttribute("src",randomImg_m)
+    img = document.getElementById('Madagascar2')
+    img.setAttribute("src",randomImg_m2)
+    console.log(randomImg_m)
+    console.log(randomImg_b)
+
     }    
